35f2ee1dec322bf982e0dee6f8b1e35ebff6b959
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-04-02 16:28:28 +0100

    Adding steps

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+# Pong game using Turtle with Python
+
+# Steps:
+# Create the screen
+# Create and move a paddle
+# Create another paddle
+# Create the ball and make it move
+# Detect collision with the wall and bounce
+# Detect collision with the paddle
+# Detect when paddle misses
+# Keep score
