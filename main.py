e42a2c08663dfe1a394d1f236774774bfd4b911d
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-04-03 00:00:03 +0100

    Finished day 22

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from score_keeping import Score_Keeping
 
 screen = Screen()
 
@@ -25,6 +26,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+score_keeping = Score_Keeping()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -37,9 +39,24 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_running = True
 
 while game_is_running:
-    time.sleep(0.01)
+    time.sleep(ball.ball_speed)
     screen.update()
     ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.xcor() > 330 and ball.distance(r_paddle) < 50 or ball.xcor() < -330 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.ball_reset()
+        score_keeping.l_score_point()
+
+    if ball.xcor() < -380:
+        ball.ball_reset()
+        score_keeping.r_score_point()
+
 
 
 screen.exitonclick()
--- venv/ball.py
+++ venv/ball.py
@@ -8,8 +8,24 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+        self.ball_speed = 0.1
+
 
     def move(self):
-        new_x = self.xcor() + 1
-        new_y = self.ycor() + 1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.ball_speed *= 0.9
+
+    def ball_reset(self):
+        self.goto(0, 0)
+        self.ball_speed = 0.1
+        self.bounce_x()
